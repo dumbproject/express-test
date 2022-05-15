687040bba064d3ed1c7b357f21985098a91538a6
zzivnuska <zzivnuska@gmail.com>
2022-05-15 05:28:32 -0700

    i am stupidddddd

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express')
 const app = express()
+const PORT = 3000 || process.env.PORT
+
 
 app.set('view engine', 'ejs')
 
-console.log('greetings earthlings\nwe running shit...\n')
+console.log('greetings earthlings\nwe runnin shit...\n')
 
 app.get('/', (req, res) => {
     console.log('here')
+    res.render("index.ejs")
     // res.status(500)
     // res.send('hi')
-    res.render("index")
 })
 
 
-app.listen(3000)
-console.log('listening on port 3000')
+// app.listen(3000)
+app.listen(PORT)
+console.log(`listening on port ${PORT}`)
